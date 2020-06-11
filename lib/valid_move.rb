f7e0b473e0f47f7fec03f5b77d4dbfc07da98b46
Sawandi Wilson <sawandiwilson@gmail.com>
2020-06-11 22:58:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,4 +5,10 @@ def valid_move?(board, index)
     return false
   end
 end
-
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
